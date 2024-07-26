9b5739fca8652aa8f6cf3afd8356fdf8dcefe170
MaxxShadow <99481513+MaxxShadow@users.noreply.github.com>
2024-07-27 00:28:53 +0200

    Update test_funkci.py

--- Lekce_7/test_funkci.py
+++ Lekce_7/test_funkci.py
@@ -6,10 +6,22 @@ import soucet_s_posunem_ver3
     ([-1, -2, -3, -4, -5, -6, -7, -8], 8, [-1, -2, -3, -4, -5, -6, -7, -8]),
     ([-1, -2, -3, -4, -5, -6, -7, -8], 4, [-1, -2, -3, -4]),
     ([-1, -2, -3, -4, -5, -6, -7, -8], 5, [-1, -2, -3, -4, -5]),
-    ([-1, -2, -3, -4, -5, -6, -7, -8], 6, [-1, -2, -3, -4, -5, -6]),
-    ([-1, -2, -3, -4, -5, -6, -7, -8], 9, [-1, -2, -3, -4, -5, -6, -7, -8])
+    ([-1, -2, -3, -4, -5, -6, -7, -8], 6, [-1, -2, -3, -4, -5, -6])
 ])
 def test_zkrat_seznam(s, d, expected):
      assert soucet_s_posunem_ver3.zkrat_seznam(s,d) == expected 
 
 
+@pytest.mark.parametrize("s, p, expected",[
+     ([-1, -2, -3, -4, -5, -6, -7, -8], 3, [-4, -5, -6, -7, -8, -1, -2, -3]),
+])
+def test_posun_seznam(s, p, expected):
+     assert soucet_s_posunem_ver3.posun_seznam(s,p) == expected
+
+
+@pytest.mark.parametrize("s, expected",[
+     ([-1, -1, -1, -1, -1, -1, 3, 3], 0),
+     ([-1, -2, -3, -4, -5, -6, -7, -8], -36),
+])
+def test_secti_seznam(s, expected):
+     assert soucet_s_posunem_ver3.secti_seznam(s) == expected 
