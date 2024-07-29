98fec22df578260c07dcc283bb097db0cac93f17
MaxxShadow <99481513+MaxxShadow@users.noreply.github.com>
2024-07-29 12:38:21 +0200

    Update test_funkci.py

--- Lekce_7/test_funkci.py
+++ Lekce_7/test_funkci.py
@@ -1,6 +1,6 @@
 import pytest
 import soucet_s_posunem_ver3 
-
+'''
 @pytest.mark.parametrize("s, d, expected", [
     ([-1, -2, -3, -4, -5, -6, -7, -8], 1, [-1]),
     ([-1, -2, -3, -4, -5, -6, -7, -8], 8, [-1, -2, -3, -4, -5, -6, -7, -8]),
@@ -21,7 +21,26 @@ def test_posun_seznam(s, p, expected):
 
 @pytest.mark.parametrize("s, expected",[
      ([-1, -1, -1, -1, -1, -1, 3, 3], 0),
-     ([-1, -2, -3, -4, -5, -6, -7, -8], -36),
+     ([-1, -2, -3, -4, -5, -6, -7, -8], -36)
 ])
 def test_secti_seznam(s, expected):
      assert soucet_s_posunem_ver3.secti_seznam(s) == expected 
+'''
+
+'''
+>> pytest -vv -rp
+'''
+
+@pytest.mark.parametrize("s1, s2, p, expected",[
+     ([-1, -2, -3, -4, -5, -6, -7, -8]
+	 ,[-1, -2, -3, -4, -5, -6, -7, -8]
+	 , 3
+	 , ([-1, -2, -3, -4, -5, -6, -7, -8],[-4, -5, -6, -7, -8, -1, -2, -3]))
+])
+def test_uprav_seznamy(s1, s2,p, expected):
+     ss1,ss2 = soucet_s_posunem_ver3.uprav_seznamy(s1, s2,p)
+     print(f"{ss1=} | {ss2=}")
+     print(expected)
+     assert soucet_s_posunem_ver3.uprav_seznamy(s1, s2,p) == expected 
+
+     
